461d95e0d4205858dcb73b21ed2f0b29e11ffc6c
Kyle Ortiz <kyleaortiz@gmail.com>
2021-08-01 15:04:48 -0500

    commit tree a0ac145c213c0b0bae2b25e055ffa94e18640c39

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [switchToggle, setSwitch] = useState(false);
+  function toggleHandler() {
+    setSwitch((switchToggle) => !switchToggle);
+  }
+  const color = switchToggle ? "red" : "blue";
+  return (
+    <button style={{ background: color }} onClick={toggleHandler}>
+      {switchToggle ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
